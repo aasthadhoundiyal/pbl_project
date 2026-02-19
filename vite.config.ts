eb2722c97b38c43b04b2abf8fc9d169123fa798e
aasthadhoundiyal <dhoundiyalaastha@gmail.com>
2026-02-19 12:17:40 +0530

    minor deploy changes

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/pbl_project/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
